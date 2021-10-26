5c48dd090ccd0d30095ff3ffc2c910e773b384db
lilalayer <90830478+lilalayer@users.noreply.github.com>
2021-10-26 11:37:02 +0300

    Add files via upload

---
+++ sixth/6.6.py
@@ -0,0 +1,7 @@
+print("Введите значение a")
+a=int(input())
+a2=a*a
+a4=a2*a2
+a8=a4*a4
+print("A**8=", a8)
+
---
+++ sixth/6.7.py
@@ -0,0 +1,9 @@
+print("Введите значение a")
+a=int(input())
+a2=a*a
+a3=a2*a
+a6=a3*a3
+a12=a6*a6
+a15=a12*a3
+print("A**15=", a15)
+
