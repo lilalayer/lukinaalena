d2e0faf9ea1414767a291d7f024fbb03ea3ca14b
lilalayer <90830478+lilalayer@users.noreply.github.com>
2021-10-26 11:35:45 +0300

    Add files via upload

---
+++ seventh/7.6.py
@@ -0,0 +1,10 @@
+print("Введите коэффициенты A1, B1, C1, A2, B2, C2")
+a1=int(input())
+b1=int(input())
+c1=int(input())
+a2=int(input())
+b2=int(input())
+c2=int(input())
+x=(c2-(b2*c1))/((b1*a2)-a1)
+y=(c1-(a1*x))/b1
+print("x=", x, "y=", y)
