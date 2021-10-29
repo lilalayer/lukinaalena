06c212f4d592d8d12de43ced31fb5ee5296ed6dc
lilalayer <90830478+lilalayer@users.noreply.github.com>
2021-10-29 13:47:38 +0300

    Add files via upload

---
+++ eighth/8.4.py
@@ -0,0 +1,5 @@
+print("Введите двузначное число")
+a=int(input())
+d=a//10
+e=a%10
+print(e, d, sep='')
---
+++ eighth/8.5.py
@@ -0,0 +1,5 @@
+print("Введите трёхзначное число")
+a=int(input())
+s=a//100
+de=a%100
+print(de, s, sep='')
